422b0be00725d4a5e7224aa965712501b340d5df
Abdoul Bass <malickya8891@gmail.com>
2021-04-07 13:45:20 -0700

    updated server.js

--- server.js
+++ server.js
@@ -22,3 +22,14 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
