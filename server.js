e531a85758e728f3dca7a4692a01ba6d1f838c20
Abdoul Bass <malickya8891@gmail.com>
2021-04-09 08:16:59 -0700

    created public folder as static so that all request route back to it

--- server.js
+++ server.js
@@ -1,35 +1,94 @@
-
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const database = require("./db/db")
 
-//======================================================================
-// This sets up the Express App
-//======================================================================
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-//==============================================================================
-// Gotta link to my assets!
-app.use(express.static('public'));
 
-//==============================================================================
-// This sets up data parsing-- Express will interpret it/format data as JSON.
-// This is required for API calls!
-//==============================================================================
+
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+
+// app.get("/notes", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/public/notes.html"));
+// })
 
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-})
+// app.route("/api/notes")
+   
+//     .get(function (req, res) {
+//         res.json(database);
+//     })
 
+    
+//     .post(function (req, res) {
+//         let jsonFilePath = path.join(__dirname, "/db/db.json");
+//         let newNote = req.body;
+
+        
+//         let highestId = 99;
+      
+//         for (let i = 0; i < database.length; i++) {
+//             let individualNote = database[i];
+
+//             if (individualNote.id > highestId) {
+              
+//                 highestId = individualNote.id;
+//             }
+//         }
+       
+//         newNote.id = highestId + 1;
+      
+//         database.push(newNote)
+
+   
+//         fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+//             if (err) {
+//                 return console.log(err);
+//             }
+//             console.log("Your note was saved!");
+//         });
+        
+//         res.json(newNote);
+//     });
+
+
+
+// app.delete("/api/notes/:id", function (req, res) {
+//     let jsonFilePath = path.join(__dirname, "/db/db.json");
+    
+//     for (let i = 0; i < database.length; i++) {
+
+//         if (database[i].id == req.params.id) {
+            
+//             database.splice(i, 1);
+//             break;
+//         }
+//     }
+    
+//     fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+//         if (err) {
+//             return console.log(err);
+//         } else {
+//             console.log("Your note was deleted!");
+//         }
+//     });
+//     res.json(database);
+// });
+
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// });
